ca1d9b9db4ba024edc731b00a3fe265220e6e798
Salman Alsuwailem <salman6111@gmail.com>
2020-03-16 10:58:14 +0300

    add deleteSubject and deleteSubject

---
+++ schoolapp/src/School/components/addSubject.js
@@ -0,0 +1,12 @@
+// export default class AddArticle extends React.Component {
+//     // Setting constructor to hold the subject object's value to be added
+//     constructor(props) {
+//       super(props);
+//       this.state = {
+//         Name: "",
+//         SubjectCode:  "",
+//         Level: "",
+//         Teacher: "",
+//         Exam: ""
+//       };
+//     }
